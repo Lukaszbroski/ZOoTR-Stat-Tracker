006a2877bb151cebc79dbe5be21d422688926bb0
Luke <archerbomb_b@hotmail.com>
2018-12-19 23:21:03 -0800

    Refactoring code

--- State.py
+++ State.py
@@ -4,40 +4,45 @@
 #this file is solely dedicated towards defining states in
 #the same way that the randomizer does in its logic and generation.
 
-class Item(object):
+class Item:
     def __init__(self, name = '', value = 0, sphere = -1, location = ''):
         self.name = name
         self.value = value
         self.sphere = sphere
         self.location = location
 
-class State(object):
+class State:
     def __init__(self):
         self.item_table = []
-        self.item_table.append(Item('Kokiri Sword', False, -1))
-        self.item_table.append(Item('Bow', 0,  -1))
-        self.item_table.append(Item('Hookshot', 0,  -1))
+        self.item_table.append(Item('Kokiri_Sword', False, -1))
+        self.item_table.append(Item('Bow', False,  -1))
+        self.item_table.append(Item('Hookshot', False,  -1))
+        self.item_table.append(Item('Longshot', False,  -1))
         self.item_table.append(Item('Hammer', False,  -1))
-        self.item_table.append(Item('Slingshot', 0,  -1))
+        self.item_table.append(Item('Slingshot', False,  -1))
         self.item_table.append(Item('Boomerang', False,  -1))
-        self.item_table.append(Item('Bombs', 0,  -1))
+        self.item_table.append(Item('Bombs', False,  -1))
+        self.item_table.append(Item('Bombchus', False,  -1))
         self.item_table.append(Item('Lens', False,  -1))
         self.item_table.append(Item('Dins', False,  -1))
-        self.item_table.append(Item('Fire Arrows', False,  -1))
-        self.item_table.append(Item('Light Arrows', False,  -1))
+        self.item_table.append(Item('Fire_Arrows', False,  -1))
+        self.item_table.append(Item('Light_Arrows', False,  -1))
         self.item_table.append(Item('Ruto', False,  -1))
-        self.item_table.append(Item('Bottle', 0,  -1))
-        self.item_table.append(Item('Wallet', 0,  -1))
+        self.item_table.append(Item('Bottle', False,  -1))
+        self.item_table.append(Item('Wallet', False,  -1))
         self.item_table.append(Item('Skulltula', 0,  -1))
-        self.item_table.append(Item('Mirror', False,  -1))
-        self.item_table.append(Item('Goron Tunic', False,  -1))
-        self.item_table.append(Item('Zora Tunic', False,  -1))
-        self.item_table.append(Item('Iron Boots', False,  -1))
-        self.item_table.append(Item('Hover Boots', False,  -1))
-        self.item_table.append(Item('Strength', 0,  -1))
-        self.item_table.append(Item('Scale', 0,  -1))
+        self.item_table.append(Item('Mirror_Shield', False,  -1))
+        self.item_table.append(Item('Goron_Tunic', False,  -1))
+        self.item_table.append(Item('Zora_Tunic', False,  -1))
+        self.item_table.append(Item('Iron_Boots', False,  -1))
+        self.item_table.append(Item('Hover_Boots', False,  -1))
+        self.item_table.append(Item('Bracelet', False,  -1))
+        self.item_table.append(Item('Silvers', False,  -1))
+        self.item_table.append(Item('Golds', False,  -1))
+        self.item_table.append(Item('Silver_Scale', 0,  -1))
+        self.item_table.append(Item('Gold_Scale', False,  -1))
         self.item_table.append(Item('Agony', False,  -1))
-        self.item_table.append(Item('Magic', 0,  -1))
+        self.item_table.append(Item('Magic', False,  -1))
         self.item_table.append(Item('Lullaby', False,  -1))
         self.item_table.append(Item('Epona', False,  -1))
         self.item_table.append(Item('Sun', False,  -1))
@@ -50,44 +55,59 @@ class State(object):
         self.item_table.append(Item('Emerald', False,  -1))
         self.item_table.append(Item('Ruby', False,  -1))
         self.item_table.append(Item('Sapphire', False,  -1))
-        self.item_table.append(Item('Forest Medallion', False,  -1))
-        self.item_table.append(Item('Fire Medallion', False,  -1))
-        self.item_table.append(Item('Water Medallion', False,  -1))
-        self.item_table.append(Item('Spirit Medallion', False,  -1))
-        self.item_table.append(Item('Shadow Medallion', False,  -1))
-        self.item_table.append(Item('Light Medallion', False,  -1))
-        self.item_table.append(Item('Ganon Entry', False,  -1))
-        temple = ['Forest Temple', 'Fire Temple', 'Water Temple', 'Shadow Temple', 'Spirit Temple', 'light temple']
+        self.item_table.append(Item('Forest_Medallion', False,  -1))
+        self.item_table.append(Item('Fire_Medallion', False,  -1))
+        self.item_table.append(Item('Water_Medallion', False,  -1))
+        self.item_table.append(Item('Spirit_Medallion', False,  -1))
+        self.item_table.append(Item('Shadow_Medallion', False,  -1))
+        self.item_table.append(Item('Light_Medallion', False,  -1))
+        self.item_table.append(Item('Ganon_Entry', False,  -1))
+        temple = ['Forest_Temple', 'Fire_Temple', 'Water_Temple', 'Shadow_Temple', 'Spirit_Temple', 'Light_Temple']
         for x in temple:
-            self.item_table.append(Item('Boss Key ' + temple[x], False,  -1))
-            self.item_table.append(Item('Small Key ' + temple[x], 0, -1))
-            self.item_table.append(Item(temple[x] + 'entry', 0 , -1))
-        self.item_table.append(Item('Bottle with Blue Fire', False,  -1))
-        self.item_table.append(Item('Weird Egg', False,  -1))
-        self.item_table.append(Item('Adult Trade', 0,  -1))
-        self.item_table.append(Item('Ocarina', 0,  -1))
-        self.item_table.append(Item('Letter', 0,  -1))
-        self.item_table.append(Item('Hearts', 3,  -1))
-        self.item_table.append(Item('GTG entry', False,  -1))
-        self.item_table.append(Item('Hyrule entry', False,  -1))
+            self.item_table.append(Item('Boss_Key_' + temple[x], False,  -1))
+            self.item_table.append(Item('Small_Key_' + temple[x], 0, -1))
+            self.item_table.append(Item(temple[x] + '_entry', False , -1))
+        self.item_table.append(Item('Bottle_with_Blue_Fire', False,  -1))
+        self.item_table.append(Item('Weird_Egg', False,  -1))
+        self.item_table.append(Item('Adult_Trade', False,  -1))
+        self.item_table.append(Item('Ocarina', False,  -1))
+        self.item_table.append(Item('Ocarina_Of_Time', False,  -1))
+        self.item_table.append(Item('Letter', False,  -1))
+        self.item_table.append(Item('Heart_Piece', False,  -1))
+        self.item_table.append(Item('Heart_Container', False,  -1))
+        self.item_table.append(Item('GTG_entry', False,  -1))
+        self.item_table.append(Item('Hyrule_entry', False,  -1))
         self.item_table.append(Item('Adult', False,  -1))
         self.item_table.append(Item('Triforce', False,  -1))
         self.item_table.append(Item('Sticks', False, -1))
+        self.item_table.append(Item('Stick_Upgrade', False,  -1))
+        self.item_table.append(Item('Nut_Upgrade', False,  -1))
         self.item_table.append(Item('Nuts', False, -1))
-        self.item_table.append(Item('Deku', False, -1))
+        self.item_table.append(Item('Deku_Shield', False, -1))
         self.item_table.append(Item('Keysanity', False, -1))
         self.item_table.append(Item('Scrubsanity', False, -1))
         self.item_table.append(Item('Keysy', False, -1,))
 
 
 
+    def has(self, item, value):
+        for x in self.item_table:
+            if self.item_table[x].name == item:
+                if self.item_table[x].value == 0 or self.item_table[x].value == False:
+                    return False
+                else:
+                    if self.item_table[x].value == value or self.item_table[x].value > value or self.item_table[x].value == True:
+                        return True
+                    if self.item_table[x].value < value:
+                        return False
+
     def has(self, item):
-        for x in item_table:
-            j =
-            x =
-            return j == x
-            j = 'j' + str.j(1)
-            j = 1 + 'j'
-            return item_table[j]
+        for x in self.item_table:
+            if self.item_table[x].name == item:
+                if self.item_table[x].value == 0 or self.item_table[x].value == False:
+                    return False
+                else: return True
+
+
 
     def get(self, item):
